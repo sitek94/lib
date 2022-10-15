865c9694b76387c5da7f0fda953eb8396944bf98
Maciek <msitkowski94@gmail.com>
2022-10-15 08:31:25 +0200

    feat: add sum

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ function isBoolean(something) {
   return typeof something === 'boolean';
 }
 
+function sum(a, b) {
+  return a + b;
+}
+
 module.exports = {
   isNumber,
   isString,
   isBoolean,
+  sum
 }
