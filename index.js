3b3cc797d37de6d7831a505f393609abae1136d2
Maciek <msitkowski94@gmail.com>
2022-10-15 10:50:49 +0200

    feat: add multiply

--- index.js
+++ index.js
@@ -18,10 +18,15 @@ function subtract(a, b) {
   return a - b;
 }
 
+function multiply(a, b) {
+  return a * b;
+}
+
 module.exports = {
   isNumber,
   isString,
   isBoolean,
   sum,
   subtract,
+  multiply
 }
